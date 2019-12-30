f53c414453712014d144a6e8d231b729a84c73b0
qist <87984115@qq.com>
2019-12-30 20:32:47 +0800

    --feature-gates="ServiceTopology=true,EndpointSlice=true" 拓扑感知服务路由 进支持kube-proxy 为service 插件

--- kubernetes.v1.17.sh
+++ kubernetes.v1.17.sh
@@ -2826,7 +2826,7 @@ KUBE_PROXY_OPTS="--logtostderr=false \\
 --ipvs-scheduler=rr \\
 --cluster-cidr=${CLUSTER_CIDR} \\
 --log-dir=${K8S_PATH}/log \\
---feature-gates="ServiceTopology=true,EndpointSlice=true" \\
+--feature-gates=ServiceTopology=true,EndpointSlice=true \\
 --metrics-bind-address 0.0.0.0 \\
 --kubeconfig=${K8S_PATH}/conf/kube-proxy.kubeconfig"
 EOF
