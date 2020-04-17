c27a59f94e9abb4df1ed11f220087960f28305f0
qist <87984115@qq.com>
2020-04-17 11:34:34 +0800

    metrics-server 修改

--- k8s-yaml/metrics-server/README.md
+++ k8s-yaml/metrics-server/README.md
@@ -1,7 +1,7 @@
 ##  metrics-server 部署说明
 ```
-# 签发证书并生成secrets 
-# deploy.sh 如果不是使用脚本生成并安装的请打开deploy.sh 修改变量改成自己环境
+# autotls 目录自动生成证书
+# tls目录 手动签发证书 签发证书并生成secrets  deploy.sh 如果不是使用脚本生成并安装的请打开deploy.sh 修改变量改成自己环境
 # metrics-server-deployment.yaml image: juestnow/metrics-server-amd64:v0.3.6 可以修改成自己私有仓库
 # 不使用自动生成secrets yaml 手动创建secrets kubectl -n kube-system create secret generic metrics-server-certs --from-file=metrics-server-key.pem --from-file=metrics-server.pem
 # 部署 metrics-server  kubectl apply -f .
---
+++ k8s-yaml/metrics-server/tls/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cat << EOF | tee ${HOST_PATH}/cfssl/k8s/metrics-server.json
+{
+  "CN": "metrics-server",
+  "hosts": [""], 
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+            "C": "CN",
+            "ST": "$CERT_ST",
+            "L": "$CERT_L",
+            "O": "$CERT_O",
+            "OU": "$CERT_OU"
+    }
+  ]
+}
+EOF
+cfssl gencert \
+        -ca=${HOST_PATH}/cfssl/pki/k8s/k8s-ca.pem \
+        -ca-key=${HOST_PATH}/cfssl/pki/k8s/k8s-ca-key.pem \
+        -config=${HOST_PATH}/cfssl/ca-config.json \
+        -profile=${CERT_PROFILE} \
+         ${HOST_PATH}/cfssl/k8s/metrics-server.json | \
+         cfssljson -bare ./metrics-server
+cat << EOF | tee ./metrics-server-secrets.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  labels:
+    k8s-app: metrics-server
+  name: metrics-server-certs
+  namespace: kube-system
+type: Opaque
+data:
+  metrics-server.pem: `cat ./metrics-server.pem|base64 | tr -d '\n'`
+  metrics-server-key.pem: `cat ./metrics-server-key.pem|base64 | tr -d '\n'`
+EOF
