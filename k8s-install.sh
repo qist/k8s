cbdad5d6ba92b3a2ea507fc44b59749c9c483845
qist <87984115@qq.com>
2021-06-11 08:53:40 +0800

    修复nginx 代理Kube-api 断开bug

--- k8s-install.sh
+++ k8s-install.sh
@@ -112,8 +112,8 @@ FEATURE_GATES_OPT="ServiceTopology=true,EndpointSlice=true,TTLAfterFinished=true
 ## kube-apiserver ha proxy 配置
 # nginx 启动进程数 auto 当前机器cpu 核心数的进程数
 CPU_NUM=4
-# 所用 镜像名字 可以自己构建  项目地址 https://github.com/qist/k8s/tree/master/dockerfile/k8s-ha-master 或者haproxy docker.io/juestnow/haproxy-proxy:2.3.3
-HA_PROXY_IMAGE="docker.io/juestnow/nginx-proxy:1.19.5"
+# 所用 镜像名字 可以自己构建  项目地址 https://github.com/qist/k8s/tree/master/dockerfile/k8s-ha-master 或者haproxy docker.io/juestnow/haproxy-proxy:2.4.0
+HA_PROXY_IMAGE="docker.io/juestnow/nginx-proxy:1.21.0"
 # pod-infra-container-image 地址
 POD_INFRA_CONTAINER_IMAGE="docker.io/juestnow/pause:3.4.1"
 #########################################################################################################################################################################
