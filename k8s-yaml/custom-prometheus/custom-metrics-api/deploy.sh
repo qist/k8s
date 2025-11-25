26addc5cdf3cab840f622bed90b48fb1bd2de2b4
qist <87984115@qq.com>
2025-11-25 16:15:21 +0800

    fix

--- k8s-yaml/custom-prometheus/custom-metrics-api/deploy.sh
+++ k8s-yaml/custom-prometheus/custom-metrics-api/deploy.sh
@@ -3,5 +3,4 @@
 kubectl apply -n monitoring -f custom-metrics-apiserver-resource-reader-cluster-role-binding.yaml
 kubectl apply -n monitoring -f custom-metrics-apiservice.yaml
 kubectl apply -n monitoring -f custom-metrics-cluster-role.yaml
-kubectl apply -n monitoring -f custom-metrics-configmap.yaml
 kubectl apply -n monitoring -f hpa-custom-metrics-cluster-role-binding.yaml
